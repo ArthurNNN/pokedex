7f50f20c7f1473143858af539e22f7acafdae8e5
ArthurNNN <nikitinarthur@gmail.com>
2021-04-30 17:08:25 +0200

    Pocedex InClass block finished

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import Logo from './Logo';
+import BestPocemon from './BestPocemon';
+import CaughtPokemon from './CaughtPokemon';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div>
+      <Logo appName='Pokedex' />
+      <BestPocemon abilities={['Anticipation', 'Adaptability', 'Run-Away']} />
+      <CaughtPokemon date={new Date().toLocaleDateString()} />
     </div>
+
   );
 }
 
 export default App;
+
+
+
+  // <div className="App">
+  //   <header className="App-header">
+  //     <img src={logo} className="App-logo" alt="logo" />
+  //     <p>
+  //       Edit <code>src/App.js</code> and save to reload. By Artur Nikitsin
+  //     </p>
+  //     <a
+  //       className="App-link"
+  //       href="https://reactjs.org"
+  //       target="_blank"
+  //       rel="noopener noreferrer"
+  //     >
+  //       Learn React
+  //     </a>
+  //   </header>
+  //   <NavBar>
+
+  //   </NavBar>
+  // </div>
