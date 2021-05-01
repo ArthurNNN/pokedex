008ea8dcc7eb77fbdbe5cda6646ac9b393c78aac
ArthurNNN <nikitinarthur@gmail.com>
2021-05-01 15:50:12 +0200

    Inclass A, B finished

--- src/App.js
+++ src/App.js
@@ -17,25 +17,3 @@ const App = () => {
 }
 
 export default App;
-
-
-
-  // <div className="App">
-  //   <header className="App-header">
-  //     <img src={logo} className="App-logo" alt="logo" />
-  //     <p>
-  //       Edit <code>src/App.js</code> and save to reload. By Artur Nikitsin
-  //     </p>
-  //     <a
-  //       className="App-link"
-  //       href="https://reactjs.org"
-  //       target="_blank"
-  //       rel="noopener noreferrer"
-  //     >
-  //       Learn React
-  //     </a>
-  //   </header>
-  //   <NavBar>
-
-  //   </NavBar>
-  // </div>
--- src/Logo.js
+++ src/Logo.js
@@ -1,11 +1,16 @@
 import React from 'react';
 
 const Logo = (props) => {
-    console.log(props);
+const logWhenClicked = () => {
+    console.log("It's a log in the logWhenClicked() function")
+
+}
+
+    // console.log(props);
     return (
         < header >
             <h1 >Welcome to the {props.appName}</h1>
-            <img src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/016.png" alt="pokemon" />
+            <img onClick={logWhenClicked} src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/016.png" alt="pokemon" />
         </header >
     );
 }
