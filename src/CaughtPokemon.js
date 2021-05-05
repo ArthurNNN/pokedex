0478e990c83fb43bc44d83cc566ede2ecc16ae35
ArthurNNN <nikitinarthur@gmail.com>
2021-05-05 21:34:02 +0200

    Exercise F,E  finished

--- src/CaughtPokemon.js
+++ src/CaughtPokemon.js
@@ -1,21 +1,26 @@
 import React, { useState } from 'react';
 
 const CaughtPokemon = (props) => {
-    // { var date = new Date().toLocaleDateString() }
 
-    const [caught, setCaught] = useState(0);
+    const listAll = ["Bulbasaur", "Ivysaur", "Venusaur", "Charmander", "Charizard", "Squirtle",
+        "Wartortle", "Charmeleon", "Blastoise", "Caterpie", "Metapod", "Butterfree", "Weedle",
+        "Kakuna", "Beedrill", "Pidgey", "Pidgeotto", "Pidgeot", "Rattata", "Raticate"];
+    var [caught, setCaught] = useState([]);
 
     const catchPokemon = () => {
-        setCaught(caught + 1);
-
+        setCaught(caught.concat(listAll[Math.floor(Math.random() * listAll.length)]));
+        console.log(caught);
     }
-
-
 
     return (
         <div>
-            <p>Caught {caught} Pokemon on {props.date}</p>
+            <p>Caught {caught.length} Pokemon on {props.date}</p>
             <button onClick={catchPokemon}>Click me!</button>
+            <ul>
+                {caught.map((item, index) => {
+                    return <li key={index}>{item}</li>;
+                })}
+            </ul>
         </div>
     );
 }
